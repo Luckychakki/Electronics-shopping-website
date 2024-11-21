01cafaae125c4ab4c53605b26258c0ba6db1dec3
Lakshmi Thirtha <48279529+Luckychakki@users.noreply.github.com>
2024-11-21 21:20:49 +0530

    Update settings.py

--- Eletronics_Shopping_System/E_shop/E_shop/settings.py
+++ Eletronics_Shopping_System/E_shop/E_shop/settings.py
@@ -135,5 +135,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'brewhaven3@gmail.com'
-EMAIL_HOST_PASSWORD = 'dsvd ghtp czfp tqcj'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
